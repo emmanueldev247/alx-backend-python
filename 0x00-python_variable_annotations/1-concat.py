4d36d01c081ca91c9a61b5dbc186a8707a3652c7
emmanueldev247 <mailemmydee@gmail.com>
2024-10-10 14:30:44 +0000

    added: Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Write a type-annotated function concat that takes a string "str1" and
+   a string "str2" as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Basic annotations - concat
+        Arg:
+          str1: str
+          str2: str
+        Return: result of concatenated a and b : str
+    """
+    return str1 + str2
