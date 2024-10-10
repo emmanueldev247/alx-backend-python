6585de06a5abdc38a7b9e828508e4d83a6d76b81
emmanueldev247 <mailemmydee@gmail.com>
2024-10-10 14:24:07 +0000

    fixed: linter errors

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-"""Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float."""
+"""Write a type-annotated function add that takes a float "a" and
+   a float "b"  as arguments and returns their sum as a float.
+"""
+
 
 def add(a: float, b: float) -> float:
-  """Basic annotations - add
-      Arg:
+    """Basic annotations - add
+        Arg:
           a: float
           b: float
-      Return: result of sum of a and b : float
-  """
-  return a + b
+        Return: result of sum of a and b : float
+    """
+    return a + b
